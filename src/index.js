57fecddfcd822b6b6931fe88537a6ecfaa1a622b
Priyam Singh <104049583+thenameispriyam@users.noreply.github.com>
2024-10-03 19:07:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDom from "react-dom/client";
 
 import App from "./App";
 
+// Username: thenameispriyam
+// Password: Priyam2217pwd
+
 // ReactDom.render(<App />, document.getElementById("root"));
+
+const pass = "priyam@4005";
+// API key : sinx21er89x
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 
